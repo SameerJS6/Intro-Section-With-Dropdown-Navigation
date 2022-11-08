328f9108b826f79b5a4db3458cb898170f0dd91a
Sameer Singh <sameersingh1234pok@gmail.com>
2022-11-08 15:33:13 +0530

    Button Ripple Effect Added

---
+++ script.js
@@ -0,0 +1,17 @@
+const Ripples = document.querySelectorAll(".ripple");
+
+Ripples.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripple = document.createElement("span");
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+    this.appendChild(ripple);
+
+    setTimeout(() => {
+      ripple.remove();
+    }, 1000);
+  });
+});
